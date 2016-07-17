569b3554dad2176d644a87837e8577aaf4735c8f
Kai Wood <kwood@kwd.io>
2016-07-17 23:17:22 +0200

    Add an icon for the toggle button

--- js/site.js
+++ js/site.js
@@ -5,10 +5,14 @@ $(function () {
   // Activate sticky sidebar
   $('.sticky').Stickyfill();
 
+  // Toggle "Show Info" / "Hide Info" button
   $('.collapse-button').on('click', function () {
-    var button = $(this);
-    var state = button.text().trim();
 
-    state == 'Show Info >' ? button.text('Hide Info >') : button.text('Show Info >');
+    var button = $(this);
+    var state = button.html().trim();
+    var up = 'Show Info <i class="fa fa-arrow-circle-down" aria-hidden="true"></i>';
+    var down = 'Hide Info <i class="fa fa-arrow-circle-up" aria-hidden="true"></i>';
+
+    state === up ? button.html(down) : button.html(up);
   });
 });
