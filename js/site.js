da65cd17838dcdfc7d5539191f376a6b28e3b045
Kai Wood <kwood@kwd.io>
2016-07-17 10:40:22 +0200

    Remove legacy image loader

--- js/site.js
+++ js/site.js
@@ -5,12 +5,7 @@ $(function () {
 
 // Use placeholder if avatar image is missing
 $(function () {
-
   $('.sticky').Stickyfill();
-
-  $('.img-avatar').one('error', function () {
-    this.src = '/img/missing.png'
-  });
 });
 
 
